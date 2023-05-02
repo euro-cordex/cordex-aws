8b67088176d45b64ab11993177b895c2482a8acd
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-05-02 12:49:07 +0200

    update parse workflow

--- workflows/parse-instance-ids.py
+++ workflows/parse-instance-ids.py
@@ -4,8 +4,23 @@ import pprint
 from pangeo_forge_cordex import parse_instance_ids
 
 
+def write_comment_body(iids):
+
+    output = f"Found {len(iids)} Dataset(s):\n"
+    output += "```\n"
+    for iid in iids:
+        output += f"{iid}\n"
+    output += "```"
+
+    with open("body.md", "w") as f:
+        f.write(output)
+    
+    return
+
+
 if __name__ == "__main__":
     ids = sys.argv[1]
-    found = parse_instance_ids(ids)
-    pprint.pprint(found)
+    iids = parse_instance_ids(ids)
+    pprint.pprint(iids)
+    write_comment_body(iids)
 
