de0006cc7ece9a5d395307c6ac5e3c66387b2e44
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2023-05-05 14:34:34 +0200

    added size estimate

--- workflows/parse-instance-ids.py
+++ workflows/parse-instance-ids.py
@@ -1,12 +1,13 @@
 import pprint
 import sys
 
-from pangeo_forge_cordex import parse_instance_ids
+from pangeo_forge_cordex import parse_instance_ids, total_size_ids
 
 
-def write_comment_body(iids):
+def write_comment_body(iids, total_size):
     output = "\n"
     output += f"Found {len(iids)} Dataset(s):\n"
+    output += f"Total size: {total_size/1.e6} MB\n"
     output += "```\n"
     for iid in iids:
         output += f"{iid}\n"
@@ -21,5 +22,7 @@ def write_comment_body(iids):
 if __name__ == "__main__":
     ids = sys.argv[1].replace("`", "")
     iids = parse_instance_ids(ids)
+    total_size = total_size_ids(ids)
     pprint.pprint(iids)
-    write_comment_body(iids)
+    pprint.pprint(total_size)
+    write_comment_body(iids, total_size)
