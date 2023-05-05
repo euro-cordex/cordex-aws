e467b25026df598935ad7caeb1487b44a0f8c164
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2023-05-05 16:10:05 +0200

    pre-commit

--- workflows/upload-s3.py
+++ workflows/upload-s3.py
@@ -1,14 +1,12 @@
-import sys
+# import sys
 
 import xarray as xr
+from pangeo_forge_cordex import logon, recipe_inputs_from_iids
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
-from pangeo_forge_cordex import logon, recipe_inputs_from_iids, parse_instance_ids
-
 
 def upload_s3(iid):
-    
     sslcontext = logon()
 
     recipe_inputs = recipe_inputs_from_iids(iid, sslcontext)
@@ -19,7 +17,10 @@ def upload_s3(iid):
 
     pattern = pattern_from_file_sequence(urls, "time", **pattern_kwargs)
     recipe = XarrayZarrRecipe(
-        pattern, xarray_concat_kwargs={"join": "exact"}, subset_inputs = {'time': 5}, **recipe_kwargs
+        pattern,
+        xarray_concat_kwargs={"join": "exact"},
+        subset_inputs={"time": 5},
+        **recipe_kwargs
     )
 
     recipe_pruned = recipe.copy_pruned()
@@ -33,11 +34,10 @@ def upload_s3(iid):
 
 
 if __name__ == "__main__":
-    #ids = sys.argv[1].replace("`", "")
-    #iids = parse_instance_ids(ids)
-    #total_size = total_size_ids(ids)
-    #pprint.pprint(iids)
-    #pprint.pprint(total_size)
-    iid = 'cordex.output.EUR-11.GERICS.ECMWF-ERAINT.evaluation.r1i1p1.REMO2015.v1.mon.tas.v20180813'
+    # ids = sys.argv[1].replace("`", "")
+    # iids = parse_instance_ids(ids)
+    # total_size = total_size_ids(ids)
+    # pprint.pprint(iids)
+    # pprint.pprint(total_size)
+    iid = "cordex.output.EUR-11.GERICS.ECMWF-ERAINT.evaluation.r1i1p1.REMO2015.v1.mon.tas.v20180813"
     upload_s3(iid)
-
