ae538696b87abfb3ea460f09edd4175d29ca18ac
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-05-07 20:33:58 +0200

    added catalog entry creation

--- workflows/upload-s3.py
+++ workflows/upload-s3.py
@@ -1,12 +1,36 @@
 # import sys
+from os import path as op
+from warnings import warn
 
+import fsspec
+import pandas as pd
 import xarray as xr
 from pangeo_forge_cordex import logon, recipe_inputs_from_iids
+from pangeo_forge_cordex.catalog import catalog_entry, path
+from pangeo_forge_cordex.parsing import project_from_iid
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
+bucket = "euro-cordex"
+df_url_template = f"https://cmip6-pds.s3.amazonaws.com/{bucket}/catalog"
 
-def upload_s3(iid):
+
+def get_url(bucket, prefix="", fs="s3"):
+    return f"{fs}://{op.join(bucket, prefix)}"
+
+
+def get_zarr_url(iid, bucket, prefix="", fs="s3"):
+    return f"{op.join(get_url(bucket, prefix, fs), path(iid))}"
+
+
+def get_catalog_url(bucket, project, prefix="catalog"):
+    if project == "CORDEX-Reklies":
+        project = "CORDEX"
+    catalog = project
+    return f"{op.join(get_url(bucket, prefix), catalog)}.csv"
+
+
+def run_recipe(iid):
     sslcontext = logon()
 
     recipe_inputs = recipe_inputs_from_iids(iid, sslcontext)
@@ -33,6 +57,50 @@ def upload_s3(iid):
     print(ds)
     print("-----------------------------------")
     print(f"Actual size: {ds.nbytes / 1.e6} MB")
+
+    return ds
+
+
+def upload_s3(iid):
+    project_id = project_from_iid(iid)
+
+    url = get_zarr_url(iid, bucket, "CMIP5", "s3")
+    entry = catalog_entry(iid, url)
+    cat_url = get_catalog_url(bucket, project_id)
+    print(f"project_id: {project_id}")
+    print(f"URL: {url}")
+    print(f"Catalog url: {cat_url}")
+    print(f"Catalog entry\n:{entry}")
+
+    # get existing catalog
+    try:
+        catalog = pd.read_csv(cat_url)
+        print(f"Found catalog: {cat_url}")
+    except Exception:
+        warn(f"Catalog does not exist yet: {cat_url}")
+        catalog = None
+    print(catalog)
+    # update catalog
+    if catalog is not None:
+        cat = pd.concat([catalog, entry], ignore_index=True)
+        if cat.duplicated().any():
+            duplicates = cat.where(cat.duplicated()).dropna()
+            raise Exception(f"Found duplicates in catalog: {duplicates}")
+        catalog = cat
+    else:
+        # start new catalog
+        catalog = entry
+
+    print("running recipe...")
+    ds = run_recipe(iid)
+
+    print("uploading to {url}")
+    target = fsspec.get_mapper(url)
+    ds.to_zarr(target, compute=True)
+
+    # write catalog
+    print(f"updating catalog at {cat_url}")
+    catalog.to_csv(cat_url, index=False)
 
 
 if __name__ == "__main__":
